d6673f9f1c119894c2b5336e32eb28436097253a
jscarlson <jcarlson238@gmail.com>
2020-05-14 18:28:37 -0400

    added documentation

--- R/dyad_funcs.R
+++ R/dyad_funcs.R
@@ -22,6 +22,7 @@
 #' @param dyad_mem1 String for variable name for first dyad member
 #' @param dyad_mem2 String for variable name for second dyad member
 #' @param data Data frame object containing dyadic data with dyad identifier variables
+#' @param posdef Logical value indicating whether or not final estimated variance-covariance matrix is forced to be positive semi-definite (note: this guards against the possibility of negative variance estimates)
 #' @return A list containing DCR standard errors and variances for model parameters
 #' @export
 dcr <- function(model, dyad_id, dyad_mem1, dyad_mem2, data, posdef = TRUE) {
@@ -90,6 +91,7 @@ dcr <- function(model, dyad_id, dyad_mem1, dyad_mem2, data, posdef = TRUE) {
 #' @param dyad_mem2 String for variable name for second dyad member
 #' @param ncore Integer specifying the number of cores to be used for parallel computation
 #' @param data Data frame object containing dyadic data with dyad identifier variables
+#' @param posdef Logical value indicating whether or not final estimated variance-covariance matrix is forced to be positive semi-definite (note: this guards against the possibility of negative variance estimates)
 #' @return A list containing DCR standard errors and variances for model parameters
 #' @export
 dcr_parallel <- function(model, dyad_id, dyad_mem1, dyad_mem2, ncore = ceiling(parallel::detectCores()/2), data, posdef=TRUE) {
@@ -160,6 +162,7 @@ dcr_parallel <- function(model, dyad_id, dyad_mem1, dyad_mem2, ncore = ceiling(p
 #' @param spec_vars String vector of variable names in model specification
 #' @param data Data frame object containing dyadic data with dyad identifier variables
 #' @param ef Optional matrix containing empirical estimating functions
+#' @param posdef Logical value indicating whether or not final estimated variance-covariance matrix is forced to be positive semi-definite (note: this guards against the possibility of negative variance estimates)
 #' @return A list containing DCR standard errors and variances for model parameters
 #' @export
 dcr_custom <- function(model, dyad_id, dyad_mem1, dyad_mem2, spec_vars, data, ef = NULL, posdef = TRUE) {
