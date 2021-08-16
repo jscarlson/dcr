0631f18f2c2cd86f590895f69619eba62ccb3088
jscarlson <jcarlson238@gmail.com>
2021-08-16 14:32:36 -0400

    DOF correction

--- R/dyad_funcs.R
+++ R/dyad_funcs.R
@@ -24,7 +24,7 @@
 #' @param posdef Logical value indicating whether or not DCR variance-covariance matrix is forced to be positive semi-definite
 #' @return A list containing DCR standard errors and variances for model parameters
 #' @export
-dcr <- function(model, dyad_mem1, dyad_mem2, data, posdef = TRUE) {
+dcr <- function(model, dyad_mem1, dyad_mem2, data, posdef = TRUE, dofcorr = FALSE) {
 
   create_dyadid <- function(data, name, dyad_mem1, dyad_mem2, directed = F) {
     dyadframe <- data[,c(dyad_mem1, dyad_mem2)]
@@ -61,8 +61,7 @@ dcr <- function(model, dyad_mem1, dyad_mem2, data, posdef = TRUE) {
 
     if (i == 1) {
       cov.mat.sum <- multiwayvcov::cluster.vcov(model, dyad.category, df_correction = FALSE)
-    }
-    else if (i != 1) {
+    } else {
       cov.mat.sum <- cov.mat.sum + multiwayvcov::cluster.vcov(model, dyad.category, df_correction = FALSE)
     }
 
@@ -78,14 +77,17 @@ dcr <- function(model, dyad_mem1, dyad_mem2, data, posdef = TRUE) {
   param.names <- colnames(V.hat)
 
   if (sum(diag(V.hat) < 0) >= 1 & posdef == TRUE) {
-
     decomp <- eigen(V.hat, symmetric = TRUE)
     pos_eigens <- pmax(decomp$values, rep.int(0, length(decomp$values)))
     V.hat <- decomp$vectors %*% diag(pos_eigens) %*% t(decomp$vectors)
-
   }
 
-  coef.var <- diag(V.hat)
+  if (dofcorr == TRUE) {
+    coef.var <- (N_dyad / (N_dyad - 1)) * diag(V.hat)
+  } else {
+    coef.var <- diag(V.hat)
+  }
+
   coef.se <- sqrt(coef.var)
   outputlst <- list(coef.se, coef.var)
 
@@ -112,7 +114,7 @@ dcr <- function(model, dyad_mem1, dyad_mem2, data, posdef = TRUE) {
 #' @param posdef Logical value indicating whether or not DCR variance-covariance matrix is forced to be positive semi-definite
 #' @return A list containing DCR standard errors and variances for model parameters
 #' @export
-dcr_sandwich <- function(model, dyad_id, dyad_mem1, dyad_mem2, data, posdef = TRUE) {
+dcr_sandwich <- function(model, dyad_id, dyad_mem1, dyad_mem2, data, posdef = TRUE, dofcorr = FALSE) {
 
   # set up starting params
   data[[dyad_mem1]] <- as.character(data[[dyad_mem1]]) # convert to character
@@ -159,83 +161,15 @@ dcr_sandwich <- function(model, dyad_id, dyad_mem1, dyad_mem2, data, posdef = TR
   }
 
   # return standard errors
-  coef.var <- diag(V.hat)
-  coef.se <- sqrt(coef.var)
-  outputlst <- list(coef.se, coef.var)
-  names(outputlst) <- c("SE", "VAR")
-  names(outputlst$SE) <- param.names
-  names(outputlst$VAR) <- param.names
-  return(outputlst)
-
-}
-
-# parallel version
-
-#' Compute dyadic clustering robust (DCR) variance estimates (parallelized)
-#'
-#' This function enables the parallel computation of DCR standard error
-#' and variance estimates.
-#'
-#' The default number of cores is set to the ceiling of half the total number of cores
-#' available.
-#'
-#' @param model Model object
-#' @param dyad_id String for name of dyad identifier variable
-#' @param dyad_mem1 String for name of first dyad member identifier variable
-#' @param dyad_mem2 String for name of second dyad member identifier variable
-#' @param ncore Integer specifying the number of cores to be used for parallel computation
-#' @param data `data.frame` object containing dyadic data, including dyad and dyad member identifier variables
-#' @param posdef Logical value indicating whether or not DCR variance-covariance matrix is forced to be positive semi-definite
-#' @return A list containing DCR standard errors and variances for model parameters
-#' @export
-dcr_parallel <- function(model, dyad_id, dyad_mem1, dyad_mem2, ncore = ceiling(parallel::detectCores()/2), data, posdef=TRUE) {
-
-  # set up starting params
-  data[[dyad_mem1]] <- as.character(data[[dyad_mem1]]) # convert to character
-  data[[dyad_mem2]] <- as.character(data[[dyad_mem2]]) # convert to character
-  data[[dyad_id]] <- as.character(data[[dyad_id]]) # convert to character
-  gp.tag <- -99
-  unique.dyad.mem <- na.omit(unique(c(data[[dyad_mem1]], data[[dyad_mem2]]))) # unique members
-  unique.dyad.mem <- unique.dyad.mem[order(unique.dyad.mem)] # order members
-  N_dyad <- length(unique.dyad.mem)
-  dyad.by.obs <- data[,c(dyad_mem1, dyad_mem2)] # create dyad matrix
-  N_cores <- parallel::detectCores() # number of cores available for computation
-  id_iter <- 1:N_dyad # list of indices for all dyads
-
-  # sum variance estimators for clustering on all dyads containing member i
-
-  dyad_cont_mem_clust <- function(i, s, unique.dyads, dyad.pairs) {
-    dyad.mem.i <- unique.dyads[i] # set member i
-    dyad.with.i <- apply(dyad.pairs, 1, function(x) as.numeric(dyad.mem.i %in% x)) # identify all dyads with member i
-    dyad.category <- dyad.with.i*gp.tag + (1-dyad.with.i)*1:nrow(dyad.pairs) # give unique group tag to dyads containing i
-    if (length(unique(dyad.category)) == 1) { # two few cluster protection
-      stop("Stop! Only one cluster!")
-    } else {
-      cov.mat <- sandwich::vcovCL(model, dyad.category, type = "HC0", multi0 = TRUE)
-    }
-  }
-  print("Computing Dyad Member CRSEs")
-  cov.mat.sum.mc <- parallel::mclapply(id_iter, dyad_cont_mem_clust, mc.cores = ncore, s = cov.mat.sum, unique.dyads = unique.dyad.mem, dyad.pairs = dyad.by.obs)
-  cov.mat.sum <- Reduce("+", cov.mat.sum.mc)
-
-  # substract repeated variance estimator for repeated dyads
-  cov.mat.sum.intermed <- cov.mat.sum - sandwich::vcovCL(model, data[, dyad_id], type = "HC0", multi0 = TRUE)
-
-  # substract HC variance estimator
-  V.hat <- cov.mat.sum.intermed - (N_dyad-2)*sandwich::vcovHC(model, type="HC0")
-
-  # force posdef
-  param.names <- colnames(V.hat)
-  if (sum(diag(V.hat) < 0) >= 1 & posdef == TRUE) {
-    decomp <- eigen(V.hat, symmetric = TRUE)
-    pos_eigens <- pmax(decomp$values, rep.int(0, length(decomp$values)))
-    V.hat <- decomp$vectors %*% diag(pos_eigens) %*% t(decomp$vectors)
+  if (dofcorr == TRUE) {
+    coef.var <- (N_dyad / (N_dyad - 1)) * diag(V.hat)
+  } else {
+    coef.var <- diag(V.hat)
   }
 
-  # return standard errors
-  coef.var <- diag(V.hat)
   coef.se <- sqrt(coef.var)
   outputlst <- list(coef.se, coef.var)
+
   names(outputlst) <- c("SE", "VAR")
   names(outputlst$SE) <- param.names
   names(outputlst$VAR) <- param.names
